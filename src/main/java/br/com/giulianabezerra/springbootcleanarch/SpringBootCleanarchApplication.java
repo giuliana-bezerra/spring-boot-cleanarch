843636fee0340e69f90a8f1108745deed7942ec7
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2023-08-14 21:32:25 -0300

    clean architecture

--- src/main/java/br/com/giulianabezerra/springbootcleanarch/SpringBootCleanarchApplication.java
+++ src/main/java/br/com/giulianabezerra/springbootcleanarch/SpringBootCleanarchApplication.java
@@ -1,4 +1,4 @@
-package br.com.giulianabezerra.springbootcleanarch.main;
+package br.com.giulianabezerra.springbootcleanarch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
